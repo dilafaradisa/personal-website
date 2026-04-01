d611f951e701bd834adc167852ff4b944c8c9e8e
dilafaradisa <faradisa2605@gmail.com>
2026-04-01 11:10:28 +0700

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+import os
 import sys
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+
+# --- CACHE BUSTING CODE START ---
+@app.context_processor
+def inject_dated_url():
+    def dated_url_for(endpoint, **values):
+        if endpoint == 'static':
+            filename = values.get('filename', None)
+            if filename:
+                # Find the absolute path to the static file
+                file_path = os.path.join(app.root_path, endpoint, filename)
+                try:
+                    # Append the last modified time as a query parameter '?v=16900000'
+                    values['v'] = int(os.stat(file_path).st_mtime)
+                except OSError:
+                    pass # If file isn't found, just ignore and proceed
+        return url_for(endpoint, **values)
+    
+    # Overrides the default url_for in Jinja templates
+    return dict(url_for=dated_url_for)
+# --- CACHE BUSTING CODE END ---
 
 @app.route('/')
 def index():
